91defffb30c0373e9cd11eac21a23ec151c161c9
Dat Nguyen <thdatealypy2001@gmail.com>
2023-02-16 21:44:46 +0700

    Fix .sql

--- DATABASE FILE/fashionstore.sql
+++ DATABASE FILE/fashionstore.sql
@@ -1,13 +1,14 @@
--- phpMyAdmin SQL
--- version 4.6.5.2
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 13-12-2023 at 11:30 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.24
+-- Generation Time: Feb 15, 2023 at 06:10 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `is_active` enum('0','1') NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -51,7 +52,7 @@ INSERT INTO `admin` (`id`, `name`, `email`, `password`, `is_active`) VALUES
 CREATE TABLE `brands` (
   `brand_id` int(100) NOT NULL,
   `brand_title` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `brands`
@@ -87,11 +88,11 @@ INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
 
 CREATE TABLE `cart` (
   `id` int(10) NOT NULL,
-  `p_id` int(10) NOT NULL, -- product id
-  `ip_add` varchar(250) NOT NULL, -- ip address
+  `p_id` int(10) NOT NULL,
+  `ip_add` varchar(250) NOT NULL,
   `user_id` int(10) DEFAULT NULL,
-  `qty` int(10) NOT NULL -- quantity
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `qty` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `cart`
@@ -108,8 +109,8 @@ INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`) VALUES
 
 CREATE TABLE `categories` (
   `cat_id` int(100) NOT NULL,
-  `cat_title` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `cat_title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `categories`
@@ -133,10 +134,10 @@ CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `qty` int(11) NOT NULL, -- quantity
-  `trx_id` varchar(255) NOT NULL, -- transaction id
-  `p_status` varchar(20) NOT NULL -- payment status
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `qty` int(11) NOT NULL,
+  `trx_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `p_status` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `orders`
@@ -158,29 +159,26 @@ CREATE TABLE `products` (
   `product_id` int(100) NOT NULL,
   `product_cat` int(11) NOT NULL,
   `product_brand` int(100) NOT NULL,
-  `product_title` varchar(255) NOT NULL,
+  `product_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_price` int(100) NOT NULL,
   `product_qty` int(11) NOT NULL,
-  `product_desc` text NOT NULL, -- description
+  `product_desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `product_image` text NOT NULL,
-  `product_keywords` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `product_keywords` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `products`
 --
+
 INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_qty`, `product_desc`, `product_image`, `product_keywords`) VALUES
-(1, 3, 3, 'Áo Thun Cổ Tròn Thần Cổ Đại Anubis', 150000, 20, 'The Uniqlo U collection is the realization of a dedicated and skilled team of international designers based at our Paris Research and Development Center led by Artistic Director Christophe Lemaire.', '9d6f5861-c510-ca00-3105-0019c5baca7b.jpg', 'áo thun, cổ tròn, anubis'),
-(2, 3, 4, 'Áo Thun Cổ Tròn Thần Cổ Đại Horus', 200000, 50, 'Skateboarding was born from the street. Top skaters from Japan and America are featured in this collection.', '89fb3f04-7659-df00-c27a-0019c03ad099.jpg', 'áo thun, cổ tròn, horus'),
-(3, 3, 5, 'Áo Sơ Mi Tay Ngắn Sợi Poly Đơn Giản Y Nguyên Bản', 150000, 50, 'Vải flannel dày hơn, vừa vặn, có thể tự tạo kiểu hoàn hảo hoặc làm lớp ngoài.', '893222ae-ddc3-cf00-bc57-0019aa36f94a.jpg', 'sơ mi,nguyên bản, tay ngắn'),
-(8, 4, 10, 'Giày Tây Cột Dây Nguyên Bản A6', 600000, 50, 'Giày Tây Cột Dây Nguyên Bản A6 2022. Da bò 100% + lót da heo.Đế cao su', '1616503181_Dr. Martens.jpg', 'giày,giày tây'),
-(11, 6, 14, 'Đồng hồ YACHT-MASTER 42', 1000000, 10, 'Vành đồng hồ xoay hai chiều với mặt số chia độ 60 phút của Yatch-Master được làm bằng vật liệu quý. Số và vạch chia độ nổi bật giữa nền đã qua xử lý làm mờ, phủ cát lấp lánh. ', 'm126233-0039_collection_upright_landscape.jpg', 'đồng hồ,rolex'),
-(12, 5, 6, 'Váy Mini Len Pha ', 600000, 50, 'Chất liệu vải ấm mang lại cảm giác cao cấp. Kiểu dáng thời trang với họa tiết caro.', '1521738f-2f90-4ee2-9f01-4b05c0583728.jpg', 'váy,caro'),
-(14, 6, 20, 'Túi xách da cá sấu Hermes', 2000000, 50, 'Vải flannel dày hơn, vừa vặn, có thể tự tạo kiểu hoàn hảo hoặc làm lớp ngoài.', 'tui-xach-hermes-birkin-da-ca-sau-tim-0-166.jpg', 'túi xách, hermes');
-
-
-
-
+(1, 3, 3, 'Áo thun cổ tròn Cổ đại Anubis', 150000, 20, 'The Uniqlo U collection is the realization of a dedicated and skilled team of international designers based at our Paris Research and Development Center led by Artistic Director Christophe Lemaire.', '9d6f5861-c510-ca00-3105-0019c5baca7b.jpg', 'áo thun, cổ tròn, anubis'),
+(2, 3, 4, 'Áo Thun Cổ Tròn Thần Cổ đại Horus', 200000, 50, 'Skateboarding was born from the street. Top skaters from Japan and America are featured in this collection.', '89fb3f04-7659-df00-c27a-0019c03ad099.jpg', 'áo thun, cổ tròn, horus'),
+(3, 3, 5, 'Áo Sơ Mi Tay Ngắn Sợi Rayon Đơn Giản Y Nguyên Bản', 150000, 50, 'Áo Sơ Mi Tay Ngắn Sợi Rayon Đơn Giản Y Nguyên Bản ', '893222ae-ddc3-cf00-bc57-0019aa36f94a.jpg', 'sơ mi, tay ngắn'),
+(8, 4, 10, 'Giày Tây cột dây Nguyên Bản', 600000, 50, 'với 100% da bò thật', '1616503181_Dr. Martens.jpg', 'giày, giày tây'),
+(11, 6, 14, 'Đồng hồ DATEJUST 36', 1000000, 10, 'Mặt đồng hồ là giao diện đặc trưng của một chiếc đồng hồ Rolex, có vai trò quan trọng về mặt nhận diện và khả năng đọc của nó. Đặc trưng bởi các vạch dấu giờ bằng vàng, ngăn các vết ố bẩn, mỗi mặt đồng hồ Rolex đều được thiết kế và sản xuất tại xưởng của hãng, chủ yếu theo phương pháp thủ công để đảm bảo sự hoàn mỹ.', 'm126233-0039_collection_upright_landscape.jpg', 'đồng hồ,rolex'),
+(12, 5, 6, 'Váy Mini Len Pha ', 600000, 50, 'Vải cao cấp cùng thiết kế không lỗi thời. Lưng thun co giãn thoải mái.', '1521738f-2f90-4ee2-9f01-4b05c0583728.jpg', 'váy,caro'),
+(14, 6, 20, 'Túi xách da cá Hermes', 2000000, 50, 'Hermes Birkin Bag là dòng túi xách cổ điển đáng sở hữu của thương hiệu thời trang hàng đầu nước Pháp.Chất liệu nổi bật với những vân da sang trọng và độc đáo. Đồng thời, item cũng trở nên cá tính hơn hẳn với chất da cá sấu sông Nile.', 'tui-xach-hermes-birkin-da-ca-sau-tim-0-166.jpg', 'túi, túi xách, hermes');
 
 -- --------------------------------------------------------
 
@@ -190,14 +188,14 @@ INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_t
 
 CREATE TABLE `user_info` (
   `user_id` int(10) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `email` varchar(300) NOT NULL,
-  `password` varchar(300) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `address1` varchar(300) NOT NULL,
-  `address2` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address1` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address2` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user_info`
@@ -268,36 +266,43 @@ ALTER TABLE `user_info`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `brand_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
   MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
   MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
 --
 -- AUTO_INCREMENT for table `user_info`
 --
 ALTER TABLE `user_info`
   MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- Constraints for dumped tables
 --
@@ -308,6 +313,7 @@ ALTER TABLE `user_info`
 ALTER TABLE `products`
   ADD CONSTRAINT `fk_product_brand` FOREIGN KEY (`product_brand`) REFERENCES `brands` (`brand_id`),
   ADD CONSTRAINT `fk_product_cat` FOREIGN KEY (`product_cat`) REFERENCES `categories` (`cat_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
